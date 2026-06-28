a9ea3801462ef8962cf08e2e1b5cf11a3aaa5d4f
NipunaDassanayake <Nipuna920@gmail.com>
2026-06-29 00:25:25 +0530

    feat: add request logger middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,32 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 
+const requestLogger = require("./middlewares/requestLogger");
+const notFoundHandler = require("./middlewares/notFoundHandler");
+const errorHandler = require("./middlewares/errorHandler");
+const healthRoutes = require("./modules/health/health.routes");
+const { sendSuccess } = require("./utils/apiResponse");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(requestLogger);
 
-const { sendSuccess } = require("./utils/apiResponse");
-
-const healthRoutes = require("./modules/health/health.routes");
-
-const errorHandler = require("./middlewares/errorHandler");
-
+app.use("/api/health", healthRoutes);
 
 app.get("/", (req, res) => {
   return sendSuccess(res, "Welcome to Rints Travels API");
 });
 
-app.use("/api/health", healthRoutes);
+app.use(notFoundHandler);
 app.use(errorHandler);
-
-
-
-//Test error route for testing error handling middleware
-app.get("/api/test-error", (req, res, next) => {
-  const error = new Error("This is a test error");
-  error.statusCode = 400;
-  next(error);
-});
 
 module.exports = app;
