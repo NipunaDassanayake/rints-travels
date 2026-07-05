74383afd1ffd3a2b71f8a46c26f4a4daddb86cbb
NipunaDassanayake <Nipuna920@gmail.com>
2026-07-05 21:15:16 +0530

    refactor: add async handler utility

--- backend/src/modules/packages/packages.controller.js
+++ backend/src/modules/packages/packages.controller.js
@@ -1,60 +1,44 @@
 const packagesService = require("./packages.service");
 const { sendSuccess } = require("../../utils/apiResponse");
 const { NotFoundError } = require("../../utils/AppError");
+const asyncHandler = require("../../utils/asyncHandler");
 
-const getAllPackages = async (req, res, next) => {
-  try {
-    const packages = await packagesService.getAllPackages(req.query);
 
-    return sendSuccess(res, "Travel packages retrieved successfully", packages);
-  } catch (error) {
-    next(error);
+const getAllPackages = asyncHandler(async (req, res) => {
+  const packages = await packagesService.getAllPackages(req.query);
+
+  return sendSuccess(res, "Travel packages retrieved successfully", packages);
+});
+
+const createPackage = asyncHandler(async (req, res) => {
+  const createdPackage = await packagesService.createPackage(req.body);
+
+  return sendSuccess(res, "Travel package created successfully", createdPackage, 201);
+});
+
+const getPackageById = asyncHandler(async (req, res) => {
+  const travelPackage = await packagesService.getPackageById(req.params.id);
+
+  if (!travelPackage) {
+    throw new NotFoundError("Travel package not found");
   }
-};
 
-const createPackage = async (req, res, next) => {
-  try {
-    const createdPackage = await packagesService.createPackage(req.body);
+  return sendSuccess(res, "Travel package retrieved successfully", travelPackage);
+});
 
-    return sendSuccess(res, "Travel package created successfully", createdPackage, 201);
-  } catch (error) {
-    next(error);
-  }
-};
-
-const getPackageById = async (req, res, next) => {
-  try {
-    const travelPackage = await packagesService.getPackageById(req.params.id);
-
-    if (!travelPackage) {
-      throw new NotFoundError("Travel package not found");
-    }
-
-    return sendSuccess(res, "Travel package retrieved successfully", travelPackage);
-  } catch (error) {
-    next(error);
-  }
-};
-
-const updatePackage = async (req, res, next) => {
-  try {
-    const updatedPackage = await packagesService.updatePackage(req.params.id, req.body);
+const updatePackage = asyncHandler(async (req, res) => {
+  const updatedPackage = await packagesService.updatePackage(req.params.id, req.body);
 
     return sendSuccess(res, "Travel package updated successfully", updatedPackage);
-  } catch (error) {
-    next(error);
-  }
-};
+  
+});
 
-const deletePackage = async (req, res, next) => {
-  try {
+const deletePackage = asyncHandler(async (req, res) => {
     await packagesService.deletePackage(req.params.id);
 
     return sendSuccess(res, "Travel package deleted successfully");
-  } catch (error) {
-    next(error);
-  }
-}; 
+  
+}); 
 
 
 module.exports = {
