24dc75a40c91c488d8cf90f6ae6e98d8530607ed
NipunaDassanayake <Nipuna920@gmail.com>
2026-08-08 23:39:06 +0530

    feat: complete payment management

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,11 +6,13 @@ const authRoutes = require("../modules/auth/auth.routes");
 const tourRequestsRoutes = require("../modules/tour-requests/tourRequests.routes");
 const tourGuidesRoutes = require("../modules/tour-guides/tourGuides.routes");
 const quotationsRoutes = require("../modules/quotations/quotations.routes");
+const paymentsRoutes = require("../modules/payments/payments.routes");
 const router = express.Router();
 
 router.use("/health", healthRoutes);
 router.use("/packages", packagesRoutes);
 router.use("/auth", authRoutes);
+router.use("/payments", paymentsRoutes);
 router.use("/quotations", quotationsRoutes);
 router.use("/tour-requests", tourRequestsRoutes);
 router.use("/tour-guides", tourGuidesRoutes);
